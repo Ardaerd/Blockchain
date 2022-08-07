bce0a64d7a3800fd5b7aedd905f4abf56a0141ac
Ardaerd <arderd@outlook.com>
2022-08-07 20:56:46 +0300

    constructor and create_block method are implemented

--- Blockchain.py
+++ Blockchain.py
@@ -1,7 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug  7 18:44:05 2022
+# Module 1 - Create a Blockchain
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
 
-@author: arda
-"""
+# Part_1 - Building a blockchain
 
+class Blockchain:
+    
+    def __init__:
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+        
+
+
+# Part_2 - Mining our Blockchain
